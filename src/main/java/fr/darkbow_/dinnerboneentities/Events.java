8a34c966b538001849b7a9d332a306f3e2ba4b80
BuildTools <unconfigured@null.spigotmc.org>
2021-05-25 16:39:57 +0200

    Dinnerbone Entities

--- src/main/java/fr/darkbow_/dinnerboneentities/Events.java
+++ src/main/java/fr/darkbow_/dinnerboneentities/Events.java
@@ -1,5 +1,6 @@
 package fr.darkbow_.dinnerboneentities;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -7,10 +8,11 @@ import org.bukkit.event.entity.EntitySpawnEvent;
 public class Events implements Listener {
     private DinnerboneEntities main;
 
-    public Events(DinnerboneEntities sampleplugin){main = sampleplugin;}
+    public Events(DinnerboneEntities dinnerboneentities){main = dinnerboneentities;}
 
     @EventHandler
     public void onEntitySpawning(EntitySpawnEvent event){
+        Bukkit.broadcastMessage("Spawn");
         if(main.getConfig().getBoolean("automatic") && main.getDinnerboneEntitiesConfig().getBoolean(event.getEntityType().name())){
             if(main.getConfig().getBoolean("RenameAlreadyRenamedEntities")){
                 event.getEntity().setCustomName("Dinnerbone");

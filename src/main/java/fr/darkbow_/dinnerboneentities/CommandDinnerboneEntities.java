bff9c108a3a1d3f86fb891ccdd243204a3887e62
BuildTools <unconfigured@null.spigotmc.org>
2021-05-27 23:42:05 +0200

    Dinnerbone Entities

--- src/main/java/fr/darkbow_/dinnerboneentities/CommandDinnerboneEntities.java
+++ src/main/java/fr/darkbow_/dinnerboneentities/CommandDinnerboneEntities.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -50,8 +51,10 @@ public class CommandDinnerboneEntities implements CommandExecutor {
             } else if(args[0].equalsIgnoreCase("help")){
                 sender.sendMessage(Objects.requireNonNull(main.getMessagesConfig().getString("HelpCommand")).replace("&", "§"));
             } else if(args[0].equalsIgnoreCase("auto") || args[0].equalsIgnoreCase("automatic") || args[0].equalsIgnoreCase("automatique")){
+                Bukkit.broadcastMessage("1 : " + main.getConfig().getBoolean("automatic"));
                 main.getConfig().set("automatic", !main.getConfig().getBoolean("automatic"));
                 main.saveDefaultConfig();
+                Bukkit.broadcastMessage("2 : " + main.getConfig().getBoolean("automatic"));
 
                 if(main.getConfig().getBoolean("global_command_action")){
                     for(World world : Bukkit.getWorlds()){

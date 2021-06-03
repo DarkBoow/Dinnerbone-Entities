aa366cfaeed356ab0dd9789254a3b43526263c13
BuildTools <unconfigured@null.spigotmc.org>
2021-06-04 01:00:41 +0200

    Dinnerbone Entities

--- src/main/java/fr/darkbow_/dinnerboneentities/DinnerboneEntities.java
+++ src/main/java/fr/darkbow_/dinnerboneentities/DinnerboneEntities.java
@@ -45,7 +45,7 @@ public class DinnerboneEntities extends JavaPlugin {
             for(World world : Bukkit.getWorlds()){
                 for(Entity entity : world.getEntities()){
                     if(!(entity instanceof Player)){
-                        if(entity.getCustomName() == null || (entity.getCustomName() != null && !entity.getCustomName().equals("Dinnerbone") && getConfig().getBoolean("RenameAlreadyRenamedEntities"))){
+                        if(entity.getCustomName() == null || (entity.getCustomName() != null && !entity.getCustomName().equals("Dinnerbone") && getConfig().getBoolean("RenameAlreadyRenamedEntities") && getDinnerboneEntitiesConfig().getBoolean(entity.getType().name()))){
                             entity.setCustomNameVisible(false);
                             entity.setCustomName("Dinnerbone");
                         }
